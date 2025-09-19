180b52a8487e2a7572b2910a9716437077f08f80
Veeramani Elumalai <veeramanielumalai2004@gmail.com>
2025-09-19 12:53:00 +0500

    Page created for task adding within a project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { addTaskForm } from "./js/addTaskDom";
 import { addProjectForm } from "./js/addProjectDom";
+import { projectTaskForm } from "./js/addTaskForProject";
+
+//document.body.append(projectTaskForm);
 
 const addTaskButton = document.querySelector("#addTask");
 addTaskButton.addEventListener("click", ()=> {
---
+++ src/js/addTaskForProject.js
@@ -0,0 +1,60 @@
+import { priorityButtonContainer } from "./priorityButtonDom";
+
+const projectTaskContainer = document.createElement("div");
+projectTaskContainer.id = "projectTaskContainer";
+
+const projectTaskText = document.createElement("h2");
+projectTaskText.textContent = "Project Task";
+
+const projectTaskForm = document.createElement("form");
+projectTaskForm.method = "get";
+
+//Containers for input
+const divTitle = document.createElement("div");
+const divDueDate = document.createElement("div");
+const divDescription = document.createElement("div");
+
+//Input Task Name
+const title = document.createElement("input");
+title.type = "text";
+title.id = "title";
+const labelForTitle = document.createElement("label");
+labelForTitle.htmlFor = "title";
+labelForTitle.textContent = "Task Name:";
+
+//Input Due Date
+const dueDate = document.createElement("input");
+dueDate.type = "date";
+dueDate.id = "dueDate";
+const labelForDueDate = document.createElement("label");
+labelForDueDate.htmlFor = "dueDate";
+labelForDueDate.textContent = "Due Date:";
+
+//Description
+const descriptionTextarea = document.createElement("textarea");
+descriptionTextarea.id = "description";
+descriptionTextarea.name = "description";
+descriptionTextarea.rows = 5;
+descriptionTextarea.cols = 40;
+const labelForDescriptionTextArea = document.createElement("label");
+labelForDescriptionTextArea.htmlFor = "description";
+labelForDescriptionTextArea.textContent = "Description:";
+
+//Submit button
+const subimtButton = document.createElement("button");
+subimtButton.type = "submit";
+subimtButton.id = "submitButton";
+subimtButton.textContent = "Submit";
+
+//Appending input elements into the container
+divTitle.append(labelForTitle, title);
+divDueDate.append(labelForDueDate, dueDate);
+divDescription.append(labelForDescriptionTextArea, descriptionTextarea);
+
+const clonedPriorityButtons = priorityButtonContainer.cloneNode(true);
+
+//Appending all divs to the form
+projectTaskForm.append(projectTaskText, divTitle, divDueDate, clonedPriorityButtons , divDescription, subimtButton);
+projectTaskContainer.append(projectTaskForm);
+
+export {projectTaskForm};
