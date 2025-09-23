7c5a3596ebb7b1f75c6d18148073909a485937f7
Veeramani Elumalai <veeramanielumalai2004@gmail.com>
2025-09-23 12:15:45 +0500

    Fix - Removed home.js and added home button functionality into index.js

--- src/index.js
+++ src/index.js
@@ -3,27 +3,41 @@ import { addProjectForm } from "./js/addProjectDom";
 //import { projectTaskForm } from "./js/addTaskForProject";
 import { defaultTasksList } from "./js/addTask";
 import { projectList } from "./js/addProject";
-import { homeButton } from "./js/home";
+import { defaultTaskContainer } from "./js/defaultTask";
+import { displayTasks } from "./js/defaultTask";
 
 //document.body.append(projectTaskForm);
 
 document.body.append(addTaskForm);
 document.body.append(addProjectForm);
+document.body.append(defaultTaskContainer);
 
 addTaskForm.style.display = "none";
 addProjectForm.style.display = "none";
+defaultTaskContainer.style.display = "none";
 
 const addTaskButton = document.querySelector("#addTask");
 addTaskButton.addEventListener("click", ()=> {
     addTaskForm.style.display = "block";
     addProjectForm.style.display = "none";
+    defaultTaskContainer.style.display = "none";
     console.log(defaultTasksList);
 });
 
 const addProjectButton = document.querySelector("#addProject");
 addProjectButton.addEventListener("click", ()=> {
     addTaskForm.style.display = "none";
+    defaultTaskContainer.style.display = "none";
     addProjectForm.style.display = "block";
     console.log(projectList);
 });  
+
+const homeButton = document.querySelector("#home");
+homeButton.addEventListener("click", ()=> {
+    addProjectForm.style.display = "none";
+    addTaskForm.style.display = "none";
+    defaultTaskContainer.style.display = "block";
+    displayTasks();
+    
+});
 
